0c25313a413c507f1f4e7780c32ea97b0556dd10
Seb-9 <56333750+Seb-9@users.noreply.github.com>
2020-08-13 10:39:46 -0600

    HyperSudoku
    Se añaden los dos modos de juego, diagonal y hyper(hypersudoku).
    Se sustituye "valid" con return  para optimizar el código.
    Se añade "cantidad" que genera cada vez un numero entre 8 y 20 para corregir el error que siempre se generaba 12 como número en la versión anterior. 
    Nombre de archivo se cambia HyperSudoku

--- TableroInicial.py
+++ TableroInicial.py
@@ -3,60 +3,61 @@ import random
 
 def HyperSudoku():
     tablero = [[0 for x in range(9)] for y in range(9)]
-
-    for i in range(9):
-        for j in range(9):
-            tablero[i][j] = 0
-
+    cantidad = random.randint(7,20)
+    modo = "hyper"
     # El range utilizado aquí es la cantidad de numeros que aparecen en el grid,con min 8 y max 20 numeros
-    for i in range(8,20):
+    for i in range(cantidad):
         # Elegir numeros aleatorios
         row = random.randrange(9)
         col = random.randrange(9)
         num = random.randrange(1, 10)
-        while (not Validacion(tablero, row, col, num) or tablero[row][col] != 0):  # Si ya está ocupado o no es valido, se corre de nuevo
+        while (not Validacion(tablero, row, col, num, modo) or tablero[row][col] != 0):  # Si ya está ocupado o no es valido, se corre de nuevo
             row = random.randrange(9)
             col = random.randrange(9)
             num = random.randrange(1, 10)
-        tablero[row][col] = num;
-
-    Printgrid(tablero)
+        tablero[row][col] = num
 
 
-def Printgrid(tablero):
-    TableTB = "|--------------------------------|"
-    TableMD = "|----------+----------+----------|"
-    print(TableTB)
-    for x in range(9):
-        for y in range(9):
-            if ((x == 3 or x == 6) and y == 0):
-                print(TableMD)
-            if (y == 0 or y == 3 or y == 6):
-                print("|", end=" ")
-            print(" " + str(tablero[x][y]), end=" ")
-            if (y == 8):
-                print("|")
-    print(TableTB)
-
-
-def Validacion(tablero, row, col, num):
+def Validacion(tablero, row, col, num, modo):
     # verifica en la fila
-    valid = True
     # verifica la columna y la fila
     for x in range(9):
-        if (tablero[x][col] == num):
-            valid = False
+        if tablero[x][col] == num:
+            return False
+        
     for y in range(9):
-        if (tablero[row][y] == num):
-            valid = False
+        if tablero[row][y] == num:
+            return False
+        
     rowsection = row // 3
     colsection = col // 3
     for x in range(3):
         for y in range(3):
             # verifica si la sección es valida
             if (tablero[rowsection * 3 + x][colsection * 3 + y] == num):
-                valid = False
-    return valid
+                return False
+    
+    if modo == "diagonal":
+        if row-col == 0:
+            for i in range (0,9):
+                if tablero[i][i] == num:
+                    return False
+        if row+col == 8:
+            for i in range (0,9):
+                if tablero[i][8-i] == num:
+                    return False
+                
+    if modo == "hyper":
+        if row != 0 and row != 4 and row != 8:
+            if col != 0 and col != 4 and col != 8:
+                xr = row//4
+                yr = col//4
+                for i in range (1,4):
+                    for j in range (1,4):
+                        if tablero[(xr*4)+i][(yr*4)+j]==num:
+                            return False
+                        
+    return True
 
 
 HyperSudoku()
