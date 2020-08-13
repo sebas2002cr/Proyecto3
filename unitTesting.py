0df4dc1b375705df5415717a1cb8cf553604aefb
Sebastian Obando Paniagua <60985278+sebas2002cr@users.noreply.github.com>
2020-08-13 15:30:08 -0600

    Add files via upload

---
+++ unitTesting.py
@@ -0,0 +1,21 @@
+from TableroInicial import*
+def test_HyperSudoku():
+    assert HyperSudoku(tablero)
+    assert row == 4
+    assert col == 5
+    assert num ==3
+
+def test_validacion():
+    assert Validacion(tablero, 1, 3, 7, "Hyper")
+    assert Validacion(tablero , 5,6,"diagonal")
+    assert tablero[2][6]==5
+    assert tablero[1 * 3 + 5][2 * 3 + 4] == 6
+
+def test_Validacion_modos():
+    assert modo=="hype"
+    assert modo=="diagonal"
+    
+
+
+
+
